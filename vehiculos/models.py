50508ee4d71c3edff93e041d0eb7de5b7afe65e5
Luis D'Esposito <luisdesposito1998@gmail.com>
2024-05-13 20:10:29 -0300

    modificacion en modelos.

--- vehiculos/models.py
+++ vehiculos/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 
 class Tipo(models.Model):
-    tipo = models.CharField(max_length= 55, unique=True)
+    tipo = models.CharField(max_length= 255, unique=True)
 
     def __str__(self) -> str:
         return self.tipo
     
 class Marcas(models.Model):
-    marca = models.CharField(max_length= 55)
+    marca = models.CharField(max_length= 255)
 
     def __str__(self) -> str:
         return self.marca
 
 class Modelos(models.Model):
-    modelo = models.CharField(max_length= 55, unique=True)
+    modelo = models.CharField(max_length= 255, unique=True)
 
     def __str__(self) -> str:
         return self.modelo
      
 class Ver_vehiculos(models.Model):
     tipo = models.ForeignKey(Tipo, on_delete=models.SET_NULL, null=True, blank=True)    
-    modelos = models.ForeignKey(Modelos, on_delete=models.SET_NULL, null=True, blank=True)
+    modelos = models.ManyToManyField(Modelos)
     marcas = models.ForeignKey(Marcas, on_delete=models.SET_NULL, null=True, blank=True)
