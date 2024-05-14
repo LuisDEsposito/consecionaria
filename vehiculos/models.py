de0227420c5990cb306ed2ae6401450a4cbf0c8e
Luis D'Esposito <luisdesposito1998@gmail.com>
2024-05-13 23:10:07 -0300

    def str del model vre_vehiculos

--- vehiculos/models.py
+++ vehiculos/models.py
@@ -22,3 +22,9 @@ class Ver_vehiculos(models.Model):
     tipo = models.ForeignKey(Tipo, on_delete=models.SET_NULL, null=True, blank=True)    
     modelos = models.ManyToManyField(Modelos)
     marcas = models.ForeignKey(Marcas, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def _str_(self) -> str:
+        return f"{self.tipo}"
+    class Meta:
+        verbose_name = "Ver Vehículo"
+        verbose_name_plural = "Ver Vehículos"
