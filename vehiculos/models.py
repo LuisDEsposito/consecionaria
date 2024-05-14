fe84c081a74fcbbe30c406ca37ca23a5eeb4b4e4
Luis D'Esposito <luisdesposito1998@gmail.com>
2024-05-14 17:21:09 -0300

    resuelto primer problema del formulario y avances en formulario hasta el  momento

--- vehiculos/models.py
+++ vehiculos/models.py
@@ -5,19 +5,29 @@ class Tipo(models.Model):
 
     def __str__(self) -> str:
         return self.tipo
+    class Meta:
+        verbose_name = "Tipo de vehiculo"
+        verbose_name_plural = "Tipos de vehiculo"
     
 class Marcas(models.Model):
     marca = models.CharField(max_length= 255)
 
     def __str__(self) -> str:
         return self.marca
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
 
 class Modelos(models.Model):
     modelo = models.CharField(max_length= 255, unique=True)
 
     def __str__(self) -> str:
         return self.modelo
-     
+    
+    class Meta:
+        verbose_name = "Modelo"
+        verbose_name_plural = "Modelos"
+
 class Ver_vehiculos(models.Model):
     tipo = models.ForeignKey(Tipo, on_delete=models.SET_NULL, null=True, blank=True)    
     modelos = models.ManyToManyField(Modelos)
--- vehiculos/views.py
+++ vehiculos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from . import models
+from django.http import HttpResponse
+from .models import Marcas
 
 def index(request):
     consultar = models.Ver_vehiculos.objects.all()
@@ -7,4 +9,18 @@ def index(request):
     return render(request, "vehiculos/index.html", contexto)
 
 def buscar_vehiculo(request):
-    return render(request, "buscar_vehiculo.html")
+    return render(request, "vehiculos/buscar_vehiculo.html")
+
+def resultado_busqueda(request):
+
+    if request.GET("marca"):
+        # mensaje = "Usted a ingresado: %r" %request.GET("marca")
+        marca_solicitada = request.Get("marca")
+        marcas_disponibles = Marcas.objects.filter(nombre_icontrains=marca_solicitada)
+        
+        return render(request, "buscar_vehiculo.html", )
+    
+    else:
+        mensaje = "Lo siento, no se encontraron resultados para tu busqueda."
+    
+    return HttpResponse(mensaje)
