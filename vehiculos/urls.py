db59dd4b25b5743daab9b7d15fcee638d66f04b5
Luis D'Esposito <luisdesposito1998@gmail.com>
2024-05-14 21:27:13 -0300

    (vehiculos)modificaciones en model

--- vehiculos/urls.py
+++ vehiculos/urls.py
@@ -6,5 +6,4 @@ app_name = "vehiculos"
 urlpatterns = [
     path("", views.index, name="index"),
     path('buscar_vehiculo/', views.buscar_vehiculo, name='buscar_vehiculo'),
-    path('resultado_busqueda/', views.resultado_busqueda, name='resultado_busqueda'),
     ]
--- vehiculos/views.py
+++ vehiculos/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
 from . import models
-from django.http import HttpResponse
-from .models import Marcas
+from vehiculos.models import Ver_vehiculos
 
 def index(request):
-    consultar = models.Ver_vehiculos.objects.all()
-    contexto = {'Ver_vehiculos': consultar}
+    consultar = Ver_vehiculos.objects.all()
+    contexto = {'vehiculos': consultar}
     return render(request, "vehiculos/index.html", contexto)
 
 def buscar_vehiculo(request):
     return render(request, "vehiculos/buscar_vehiculo.html")
 
-def resultado_busqueda(request):
+# def resultado_busqueda(request):
 
-    if request.GET("marca"):
-        # mensaje = "Usted a ingresado: %r" %request.GET("marca")
-        marca_solicitada = request.Get("marca")
-        marcas_disponibles = Marcas.objects.filter(nombre_icontrains=marca_solicitada)
+#     if request.get("marca"):
+#         # mensaje = "Usted a ingresado: %r" %request.GET("marca")
+#         marca_solicitada = request.get("marca")
+#         marcas_disponibles = Marcas.objects.filter(nombre_icontrains=marca_solicitada)
         
-        return render(request, "buscar_vehiculo.html", )
+#         return render(request, "buscar_vehiculo.html", )
     
-    else:
-        mensaje = "Lo siento, no se encontraron resultados para tu busqueda."
+#     else:
+#         mensaje = "Lo siento, no se encontraron resultados para tu busqueda."
     
-    return HttpResponse(mensaje)
+#     return HttpResponse(mensaje)
