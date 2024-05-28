d6c7502b4041a08a004230ad8908dc0c2981438d
Luis D'Esposito <luisdesposito1998@gmail.com>
2024-05-28 20:56:23 -0300

    simplificados las views

--- vehiculos/urls.py
+++ vehiculos/urls.py
@@ -1,20 +1,36 @@
 from django.urls import path
 from vehiculos.views import (
-    index,
-    lista_vehiculos,
-    publicar_vehiculo,
-    detalle_vehiculo,
-    editar_vehiculo,
-    eliminar_vehiculo,
-    )
+    Ver_vehiculosCreate, Ver_vehiculosList, 
+    Ver_vehiculosDetail, Ver_vehiculosUpdate, 
+    Ver_vehiculosDelete,)
 
 app_name = "vehiculos"
 
+
+from vehiculos.views import (
+    index,
+    # lista_vehiculos,
+    # publicar_vehiculo,
+    # detalle_vehiculo,
+    # editar_vehiculo,
+    # eliminar_vehiculo,
+    )
+
+
 urlpatterns = [
     path("", index, name="index"),
-    path("lista/vehiculos", lista_vehiculos, name="lista_vehiculos"), 
-    path('vehiculos/publicar', publicar_vehiculo, name='publicar_vehiculo'),
-    path('vehiculo/detalle/<int:pk>', detalle_vehiculo, name='detalle_vehiculo'),
-    path('vehiculo/editar/<int:pk>', editar_vehiculo, name='editar_vehiculo'),
-    path('vehiculo/eliminar/<int:pk>', eliminar_vehiculo, name='eliminar_vehiculo'),
+    # path("lista/vehiculos", lista_vehiculos, name="lista_vehiculos"), 
+    # path('vehiculos/publicar', publicar_vehiculo, name='publicar_vehiculo'),
+    # path('vehiculo/detalle/<int:pk>', detalle_vehiculo, name='detalle_vehiculo'),
+    # path('vehiculo/editar/<int:pk>', editar_vehiculo, name='editar_vehiculo'),
+    # path('vehiculo/eliminar/<int:pk>', eliminar_vehiculo, name='eliminar_vehiculo'),
+]
+
+# VISTAS BASADAS EN CLASES
+urlpatterns += [
+    path("lista/vehiculos", Ver_vehiculosList.as_view(), name = "lista_vehiculos"),
+    path('vehiculos/publicar', Ver_vehiculosCreate.as_view(), name='publicar_vehiculo'),
+    path('vehiculo/detalle/<int:pk>', Ver_vehiculosDetail.as_view(), name='detalle_vehiculo'),
+    path('vehiculo/editar/<int:pk>', Ver_vehiculosUpdate.as_view(), name='editar_vehiculo'),
+    path('vehiculo/eliminar/<int:pk>', Ver_vehiculosDelete.as_view(), name='eliminar_vehiculo'),
 ]
--- vehiculos/views.py
+++ vehiculos/views.py
@@ -1,55 +1,108 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from vehiculos.models import Ver_vehiculos, Marcas
 from vehiculos.forms import Ver_vehiculosform
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-
+# INICIO
 def index(request):
     return render(request, "vehiculos/index.html")
 
 
-def lista_vehiculos(request):
-    busqueda = request.GET.get("busqueda")
-    if busqueda:
-        print(busqueda)
-        consulta = Ver_vehiculos.objects.filter(marcas__icontains=busqueda)
-    else:
-        consulta = Ver_vehiculos.objects.all()
-    contexto = {'vehiculos': consulta}
-    return render(request, "vehiculos/lista_vehiculos.html", contexto)
+# LISTA
+class Ver_vehiculosList(ListView):
+    model = Ver_vehiculos
+    context_object_name = "vehiculos"
+    template_name = "vehiculos/lista_vehiculos.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        busqueda = self.request.GET.get("busqueda")
+        if busqueda:
+            queryset = Ver_vehiculos.objects.filter(marcas__icontains=busqueda)
+        return queryset
 
 
-def publicar_vehiculo(request):
-    if request.method == "POST":
-        form = Ver_vehiculosform(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect ("vehiculos:lista_vehiculos")
-    else:
-        form = Ver_vehiculosform()
-    return render (request, "vehiculos/lista_vehiculos_form.html", {"form": form})
+# PUBLICAR VEHICULO:
+class Ver_vehiculosCreate(CreateView):
+    model = Ver_vehiculos
+    form_class = Ver_vehiculosform
+    template_name = "vehiculos/lista_vehiculos_form.html"
+    success_url = reverse_lazy("vehiculos:lista_vehiculos")
+
+#VER DETALLE
+class Ver_vehiculosDetail(DetailView):
+    model = Ver_vehiculos
+    context_object_name = "vehiculo"
+    template_name = "vehiculos/lista_vehiculos_detalle.html"
+
+#ACTUALIZAR DATOS.
+class Ver_vehiculosUpdate(UpdateView):
+    model = Ver_vehiculos
+    form_class = Ver_vehiculosform
+    template_name = "vehiculos/lista_vehiculos_form.html"
+    success_url = reverse_lazy("vehiculos:lista_vehiculos")
+
+# ELIMINAR DATOS:
+class Ver_vehiculosDelete(DeleteView):
+    model = Ver_vehiculos
+    context_object_name = "vehiculo"
+    template_name = "vehiculos/lista_vehiculos_confirmar_eliminar.html"
+    success_url = reverse_lazy("vehiculos:lista_vehiculos")
 
 
-def detalle_vehiculo(request, pk: int):
-    consulta = Ver_vehiculos.objects.get(id=pk)
-    contexto = {"vehiculo": consulta}
-    return render(request, "vehiculos/lista_vehiculos_detalle.html", contexto)
+# FORMATO VIEJO EN CASO DE FALLAS.
+
+# LISTA
+# def lista_vehiculos(request):
+#     busqueda = request.GET.get("busqueda")
+#     if busqueda:
+#         print(busqueda)
+#         consulta = Ver_vehiculos.objects.filter(marcas__icontains=busqueda)
+#     else:
+#         consulta = Ver_vehiculos.objects.all()
+#     contexto = {'vehiculos': consulta}
+#     return render(request, "vehiculos/lista_vehiculos.html", contexto)
 
 
-def editar_vehiculo(request, pk : int):
-    consulta = Ver_vehiculos.objects.get(id=pk)
-    if request.method == "POST":
-        form = Ver_vehiculosform(request.POST, instance = consulta)
-        if form.is_valid():
-            form.save()
-            return redirect ("vehiculos:lista_vehiculos")
-    else:
-        form = Ver_vehiculosform(instance=consulta)
-    return render (request, "vehiculos/lista_vehiculos_form.html", {"form": form})
+# PUBLICAR VEHICULO:
+# def publicar_vehiculo(request):
+#     if request.method == "POST":
+#         form = Ver_vehiculosform(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect ("vehiculos:lista_vehiculos")
+#     else:
+#         form = Ver_vehiculosform()
+#     return render (request, "vehiculos/lista_vehiculos_form.html", {"form": form})
 
 
-def eliminar_vehiculo(request, pk : int):
-    consulta = Ver_vehiculos.objects.get(id=pk)
-    if request.method == "POST":
-        consulta.delete()
-        return redirect("vehiculos:lista_vehiculos")
-    return render(request, "vehiculos/lista_vehiculos_confirmar_eliminar.html", {"object" : consulta})
+#VER DETALLE
+# def detalle_vehiculo(request, pk: int):
+#     consulta = Ver_vehiculos.objects.get(id=pk)
+#     contexto = {"vehiculo": consulta}
+#     return render(request, "vehiculos/lista_vehiculos_detalle.html", contexto)
+
+
+# ACTUALIZAR DATOS.
+# def editar_vehiculo(request, pk : int):
+#     consulta = Ver_vehiculos.objects.get(id=pk)
+#     if request.method == "POST":
+#         form = Ver_vehiculosform(request.POST, instance = consulta)
+#         if form.is_valid():
+#             form.save()
+#             return redirect ("vehiculos:lista_vehiculos")
+#     else:
+#         form = Ver_vehiculosform(instance=consulta)
+#     return render (request, "vehiculos/lista_vehiculos_form.html", {"form": form})
+
+
+# ELIMINAR DATOS: 
+# def eliminar_vehiculo(request, pk : int):
+#     consulta = Ver_vehiculos.objects.get(id=pk)
+#     if request.method == "POST":
+#         consulta.delete()
+#         return redirect("vehiculos:lista_vehiculos")
+#     return render(request, "vehiculos/lista_vehiculos_confirmar_eliminar.html", {"object" : consulta})
