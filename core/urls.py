ab0e4aad088283f174a9f4405593db3b4b9cc0d7
Luis D'Esposito <luisdesposito1998@gmail.com>
2024-05-29 19:44:46 -0300

    creado el logout

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from core.views import index, CustomLoginView
 
 
@@ -6,5 +7,6 @@ app_name = "core"
 
 urlpatterns = [
     path("", index, name="index"),
-    path("login/", CustomLoginView.as_view(), name="login")
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(template_name = "core/logout.html"), name="logout"),
     ]
