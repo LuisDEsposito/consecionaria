56270acb860aa18bbf5f5c0e1d6987e1758d9d52
Luis D'Esposito <luisdesposito1998@gmail.com>
2024-05-28 21:51:35 -0300

    creado el login

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+from core.views import index, CustomLoginView
+
 
 app_name = "core"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-
+    path("", index, name="index"),
+    path("login/", CustomLoginView.as_view(), name="login")
     ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from .forms import CustomAuthenticationForm
 
 def index(request):
     return render(request, "core/index.html")
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "core/login.html"
+
+
