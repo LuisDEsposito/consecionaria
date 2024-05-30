f33322c37f009e88db5311feb1d9646cabbe3b42
Luis D'Esposito <luisdesposito1998@gmail.com>
2024-05-30 20:01:03 -0300

    mejoras del panel de administración

--- vehiculos/admin.py
+++ vehiculos/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from .models import Ver_vehiculos, Marcas, Modelos, Tipo, Descripcion, Precio
 
-from . import models
+class VehiculosAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        "marcas", "modelos",
+        "tipo", "fecha_creacion",
+        "valor_dolares"
+        )
+    list_display_links = ()
+    list_filter = ("marcas",)
+    date_hierarchy = "fecha_creacion"
+    
 
-admin.site.register(models.Ver_vehiculos)
-admin.site.register(models.Marcas)
-admin.site.register(models.Modelos)
-admin.site.register(models.Tipo)
-admin.site.register(models.Precio)
-admin.site.register(models.Descripcion)
+admin.site.register(Ver_vehiculos, VehiculosAdmin)
+admin.site.register(Marcas)
+admin.site.register(Modelos)
+admin.site.register(Tipo)
+admin.site.register(Precio)
+admin.site.register(Descripcion)
