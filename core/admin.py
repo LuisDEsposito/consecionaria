a9f095375c99f5c4b607b7a2d803e48bee14adb0
Luis D'Esposito <luisdesposito1998@gmail.com>
2024-05-31 00:19:37 -0300

    creado la opción añadir avatar

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Avatar_usuario
 
 # Register your models here.
+admin.site.register(Avatar_usuario)
+
+class Avatar_usuarioAdmin(admin.ModelAdmin):
+    list_display = (
+        "username"
+        "email"
+        "profile_image"
+    )
--- core/models.py
+++ core/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
-class avatar_usuario(models.Model):
+class Avatar_usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name= "Avatar", null=True)
     avatar = models.ImageField(upload_to="avatares", null=True, blank=True)
+    
+    def __str__(self) -> str:
+        if self.usuario:
+            return self.usuario.username
+        return "Sin usuario"
+
+    class Meta:
+        verbose_name = "Avatar de usuario"
+        verbose_name_plural = "Avatares de usuarios"
+
+
+
